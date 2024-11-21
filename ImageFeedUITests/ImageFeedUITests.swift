a1e977029c0fc8741d5ff240456fe5a0cdd8d19e
nex77qoz <nex77qoz@gmail.com>
2024-11-21 17:23:51 +0300

    Fixed UI tests

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -10,12 +10,20 @@ import XCTest
 final class ImageFeedUITests: XCTestCase {
     let app = XCUIApplication()
     
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    override func tearDownWithError() throws {
+        app.terminate()
+    }
+    
     func testAuth() throws {
-        let app = XCUIApplication()
         app.buttons["Войти"].tap()
         
         let webViewsQuery = app.webViews.webViews.webViews
-        sleep(3)
+        sleep(10)
         
         webViewsQuery/*@START_MENU_TOKEN@*/.textFields["Email address"]/*[[".otherElements[\"Connect ImageFeed + Unsplash | Unsplash\"].textFields[\"Email address\"]",".textFields[\"Email address\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         webViewsQuery/*@START_MENU_TOKEN@*/.textFields["Email address"]/*[[".otherElements[\"Connect ImageFeed + Unsplash | Unsplash\"].textFields[\"Email address\"]",".textFields[\"Email address\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("<email>")
@@ -29,46 +37,43 @@ final class ImageFeedUITests: XCTestCase {
         let tablesQuery = app.tables
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
         
-        XCTAssertTrue(cell.waitForExistence(timeout: 5))
+        XCTAssertTrue(cell.waitForExistence(timeout: 10))
     }
-    func testList() throws {
-                
-    }
-    
+
     func testFeed() throws {
-        let tablesQuery = app.tables
-        
-        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
-        cell.swipeUp()
-        
-        sleep(2)
-        
-        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
-   
-        cellToLike.buttons["No Active"].tap()
-        sleep(2)
-        cellToLike.buttons["Active"].tap()
-        
-        sleep(2)
-        
-        cellToLike.tap()
-        
-        sleep(2)
-        
-        let image = app.scrollViews.images.element(boundBy: 0)
-        // Zoom in
-        image.pinch(withScale: 3, velocity: 1) // zoom in
-        // Zoom out
-        image.pinch(withScale: 0.5, velocity: -1)
-        
-        let navBackButtonWhiteButton = app.buttons["Backward"]
-        navBackButtonWhiteButton.tap()
-    }
+            let tablesQuery = app.tables
+            
+            let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+            cell.swipeUp()
+            
+            sleep(2)
+            
+            let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+       
+            cellToLike.buttons["No Active"].tap()
+            sleep(2)
+            cellToLike.buttons["Active"].tap()
+            
+            sleep(2)
+            
+            cellToLike.tap()
+            
+            sleep(2)
+            
+            let image = app.scrollViews.images.element(boundBy: 0)
+            // Zoom in
+            image.pinch(withScale: 3, velocity: 1)
+            // Zoom out
+            image.pinch(withScale: 0.5, velocity: -1)
+            
+            let navBackButtonWhiteButton = app.buttons["Backward"]
+            navBackButtonWhiteButton.tap()
+        }
     
     func testProfile() throws {
         sleep(3)
         app.tabBars.buttons.element(boundBy: 1).tap()
-       
+        
         XCTAssertTrue(app.staticTexts["Maxim Babkin"].exists)
         XCTAssertTrue(app.staticTexts["@nex77qoz"].exists)
         
